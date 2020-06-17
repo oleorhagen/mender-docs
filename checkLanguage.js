a56f19cd4b21656b9da302c3bb3707a3041c3286
Ole Petter <ole.orhagen@northern.tech>
2020-06-17 13:13:30 +0200

    Add 'checkLanguage.js' prototype
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

---
+++ checkLanguage.js
@@ -0,0 +1,53 @@
+#! env /usr/bin/node
+
+var vfile = require('to-vfile')
+var report = require('vfile-reporter')
+var unified = require('unified')
+var parse = require('remark-parse')
+var stringify = require('remark-stringify')
+var remark2retext = require('remark-retext')
+var english = require('retext-english')
+var equality = require('retext-equality')
+
+// Spelling
+var retext = require('retext')
+var spell = require('retext-spell')
+var dictionary = require('dictionary-en')
+
+// Readability
+var readability = require('retext-readability')
+
+// Passive voice
+var passive = require('retext-passive')
+
+// a vs an
+var indefiniteArticle = require('retext-indefinite-article')
+
+if (process.argv.length != 3) {
+    console.error("Usage checkLanguage <file>")
+    return
+}
+
+var file = vfile.readSync({path: process.argv[2]})
+
+unified()
+    .use(parse)
+    .use(
+        remark2retext,
+        unified()
+            .use(english)
+            // .use(equality)
+            // .use(passive)
+            // .use(indefiniteArticle)
+            // .use(spell, dictionary)
+            .use(readability, {age: 18})
+    )
+    .use(stringify)
+    .process(file, function(err, file) {
+        // console.error(report(err || file))
+        // console.error(file.messages)
+        for (message of file.messages) {
+            console.error("[Warning]: Hard to read sentence: " + message.name + ":" + message.line + ":" + message.column)
+            console.error(message.actual)
+        }
+    });
